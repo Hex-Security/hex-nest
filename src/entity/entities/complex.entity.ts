925bf3d6e515c3cb7795cf90a2e2a3d7f40e22b5
ortegach <chrortegita@gmail.com>
2024-04-20 10:53:38 -0700

    starting

---
+++ src/entity/entities/complex.entity.ts
@@ -0,0 +1,12 @@
+
+export class Complex {
+    name: string;
+    admin_id: string;
+    addreSs: string;
+    city: string;
+    state: string;
+    zip: string;
+
+    created_at: Date;
+    updated_at: Date;
+}
