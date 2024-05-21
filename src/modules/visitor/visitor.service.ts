f2166d4dbe83dc4f1e71a7a339f9e7c0cf7b11e5
ortegach <chrortegita@gmail.com>
2024-05-20 19:01:48 -0700

    visitor service

--- src/modules/visitor/visitor.service.ts
+++ src/modules/visitor/visitor.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository } from 'typeorm';
+import { VisitorType } from 'src/shared/enum/visitor-type.enum';
+import { DeleteResult, In, Repository, UpdateResult } from 'typeorm';
 import { Visitor } from '../entity/entities/visitor.entity';
 
 @Injectable()
@@ -13,23 +14,58 @@ export class VisitorService {
     return this.visitor_repository.find();
   }
 
-  async findOne(uid: string): Promise<Visitor> {
-    return this.visitor_repository.findOne({ where: { uid } });
+  async findOne(visitor_id: string): Promise<Visitor> {
+    return this.visitor_repository.findOne({ where: { id: visitor_id } });
   }
 
-  async findMany(uids: string[]): Promise<Visitor[]> {
-    return this.visitor_repository.find({ where: { uid: In(uids) } });
+  async findMany(visitor_ids: string[]): Promise<Visitor[]> {
+    return this.visitor_repository.find({ where: { id: In(visitor_ids) } });
   }
 
-  async update(id: string, visitor: Partial<Visitor>): Promise<Visitor> {
-    return this.visitor_repository.save({ ...visitor, id });
+  async update(
+    visitor_id: string,
+    visitor: Partial<Visitor>,
+  ): Promise<UpdateResult> {
+    return this.visitor_repository.update(visitor_id, visitor);
   }
 
-  async remove(uid: string): Promise<Visitor> {
-    const visitor: Visitor = await this.visitor_repository.findOne({
-      where: { uid },
+  async delete(user_id: string): Promise<DeleteResult> {
+    return this.visitor_repository.delete(user_id);
+  }
+
+  async findVisitorByPhone(phone: string): Promise<Visitor> {
+    return this.visitor_repository.findOne({ where: { phone } });
+  }
+
+  async findVisitorsByType(type: VisitorType): Promise<Visitor[]> {
+    return this.visitor_repository.find({ where: { type } });
+  }
+
+  async findVisitorsByVehicle(vehicle_id: string): Promise<Visitor[]> {
+    return this.visitor_repository.find({
+      where: { vehicles: { id: vehicle_id } },
+      relations: ['vehicles'],
     });
+  }
 
-    return this.visitor_repository.remove(visitor);
+  async findVisitorsByAccess(access_id: string): Promise<Visitor[]> {
+    return this.visitor_repository.find({
+      where: { accesses: { id: access_id } },
+      relations: ['accesses'],
+    });
+  }
+
+  async findVisitorsByHouse(house_id: string): Promise<Visitor[]> {
+    return this.visitor_repository.find({
+      where: { accesses: { house: { id: house_id } } },
+      relations: ['accesses'],
+    });
+  }
+
+  async findVisitorsByComplex(complex_id: string): Promise<Visitor[]> {
+    return this.visitor_repository.find({
+      where: { accesses: { house: { complex: { id: complex_id } } } },
+      relations: ['accesses'],
+    });
   }
 }
