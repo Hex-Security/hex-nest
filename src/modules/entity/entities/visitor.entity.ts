26fc561a2495e5a20f7388e8a31c8c521b3f1f0a
ortegach <chrortegita@gmail.com>
2024-05-30 18:01:10 -0700

    added tables missing initial data

--- src/modules/entity/entities/visitor.entity.ts
+++ src/modules/entity/entities/visitor.entity.ts
@@ -1,11 +1,18 @@
 import { VisitorType } from 'src/shared/enum/visitor-type.enum';
-import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { Access } from './access.entity';
 import { Vehicle } from './vehicle.entity';
 
 @Entity()
 export class Visitor {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
@@ -25,4 +32,10 @@ export class Visitor {
 
   @OneToMany(() => Access, (access) => access.visitor)
   accesses: Access[];
+
+  @CreateDateColumn()
+  created_at: string;
+
+  @UpdateDateColumn()
+  updated_at: string;
 }
